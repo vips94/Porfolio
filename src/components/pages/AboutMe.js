259f0207522f022610b2ed8bd2f00a574db3ebc3
Vipin Kumar <vipinkumar@Vipins-MacBook-Air.local>
2021-11-08 11:01:32 +0530

    first deploy

--- src/components/pages/AboutMe.js
+++ src/components/pages/AboutMe.js
@@ -18,7 +18,7 @@ const AboutMe = ({children})=>{
 
                 </div>
                 <div className={classes.aboutMe__grid__photo}>
-                    <img  src="/images/profilecopy.jpg" alt=""/>
+                    <img  src="/images/profilePhoto.jpg" alt=""/>
                 </div>
             </div>
         </div>

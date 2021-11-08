e5377f57cc7393077170d6d3206b92e8bd23451c
Vipin Kumar <vipinkumar@Vipins-MacBook-Air.local>
2021-11-08 19:57:43 +0530

    final

--- src/components/common/PdfView.js
+++ src/components/common/PdfView.js
@@ -1,8 +1,9 @@
-import React,{useRef,useEffect} from 'react';
+import React,{useState,useRef,useEffect} from 'react';
 import WebViewer from '@pdftron/webviewer';
 import classes from './PdfView.module.scss';
 
 const PdfView = (props)=>{
+    const [isLoaded,setIsloaded] = useState(false);
     const viewer = useRef(null)
 
     useEffect(()=>{
@@ -13,17 +14,20 @@ const PdfView = (props)=>{
             },
             viewer.current,
             ).then((instance) => {
-                console.log("showing pdf");
-                //const { documentViewer } = instance.Core;
-                // you can now call WebViewer APIs here...
+                setIsloaded(true);
+                console.log(isLoaded);
             }).catch((err)=>console.log(err));
-        }, []);
+        }, [isLoaded]);
 
     return(
         <div className={classes.container} onClick={props.onClick}>
             <div className={classes.container__view} onClick = {(event) => event.stopPropagation()}>
                 <a href= {process.env.PUBLIC_URL+"/documents/resume.pdf"} download>DOWNLOAD RESUME</a>
-                <div ref={viewer} className={classes.container__view__pdf} onClick = {(event) => event.stopPropagation()}/>
+                <div ref={viewer} className={classes.container__view__pdf} onClick = {(event) => event.stopPropagation()}>
+                {
+                    !isLoaded && <p>Loading...</p> 
+                }
+                </div>
             </div>
         </div>
     );
